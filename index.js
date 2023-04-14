c3fa27e0e95d386c29c78c56fdd0500bcb3f902c
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-14 20:57:06 +0300

    fhadf

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-
 const app = express()
 const PORT = process.env.PORT || 4000
+
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+
+const swaggerDocument = YAML.parse(file)
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors())
 app.use(express.json())
@@ -11,6 +20,9 @@ app.use(express.json())
 app.get('/users',(req,res) => {
     return res.json({message: 'Get all Users'})  
 })
+
+
+
 
 
 app.listen(PORT)
