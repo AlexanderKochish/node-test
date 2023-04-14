9d07820bd9851029acde605c822a87d11e2845d8
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-14 19:35:29 +0300

    halva

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.use(express.json())
+
 app.get('/users',(req,res) => {
     return res.json({message: 'Get all Users'})  
 })
