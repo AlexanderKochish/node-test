449d48ea908844a4c9082d297048e7270b26ecd0
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-14 18:24:09 +0300

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.get('/users',(req,res) => {
+    return res.json({message: 'Get all Users'})  
+})
+
+
+app.listen(PORT, console.log(`Server run`))
