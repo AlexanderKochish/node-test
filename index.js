aa7f43b35c1020d88ec91892380cade7407dfcec
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-14 20:38:46 +0300

    ggjhgfdss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -12,4 +13,4 @@ app.get('/users',(req,res) => {
 })
 
 
-app.listen(`https://node-test-server-vbvw.onrender.com`)
+app.listen(PORT)
