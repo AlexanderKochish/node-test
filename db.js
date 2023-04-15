9fc5688c3d4baeaa7108e63f8a2b53c8ee93d210
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-15 23:00:37 +0300

    first node test

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ const sequelize = new Sequlize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD,{
-        dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        dialect: 'postgres'
     }
 )
 
