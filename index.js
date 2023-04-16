d162272ae2c31a84dd6825942e5f50033389f9c5
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-16 15:36:36 +0300

    uuuuiiiiiddddd

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ require('dotenv').config()
 const cors = require('cors')
 const app = express()
 const router = require('./routes/router')
-const sequelize = require('./db')
 const PORT = 3000
-
+const sequelize = require('./db')
+const fs = require("fs")
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yaml')
 
 app.use(cors())
 app.use(express.json())
+var options = {
+    explorer: true
+};
 app.use(router)
+const file  = fs.readFileSync('./swagger.js','utf-8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 const start = async() => {
     try {
