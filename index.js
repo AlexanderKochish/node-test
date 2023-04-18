fbd6f75696dd0a41273181c3955e8b02f5bdc4eb
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-18 23:38:17 +0300

    hgdffasfg

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const app = express()
 require('dotenv').config()
 const cors = require('cors')
 const sequelize = require('./db')
 const router = require('./routes/router')
 const PORT = 3000
-const app = express()
 // swagger
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs')
@@ -14,12 +14,12 @@ app.use(cors())
 app.use(express.json())
 app.use("/",router)
 
-
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const start = async() => {
     try {
         await sequelize.authenticate();
         await sequelize.sync()
-        app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+        
         app.listen(PORT, () => console.log('Server listening on port'))
     } catch (error) {
         console.log(error.message)
