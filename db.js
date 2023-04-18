26488d02c967b34a8cc1efc5e9f83c55e3727f8f
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-18 23:22:24 +0300

    aliluya

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const pg = require('pg')
 
 
 const sequelize = new Sequlize(
-    process.env.PGDATABASE,
-    process.env.PGUSER,
-    process.env.PGPASSWORD,{
-        host: process.env.PGHOST,
-        port: process.env.PGPORT,
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,{
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         dialectModule: pg,
         dialect: 'postgres',
         logging: false
--- index.js
+++ index.js
@@ -5,28 +5,25 @@ const app = express()
 const router = require('./routes/router')
 const PORT = 3000
 const sequelize = require('./db')
-const fs = require("fs")
+// swagger
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yaml')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 app.use(cors())
 app.use(express.json())
-var options = {
-    explorer: true
-};
-app.use(router)
-const file  = fs.readFileSync('./swagger.js','utf-8')
-const swaggerDocument = YAML.parse(file)
+app.use("/",router)
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 const start = async() => {
     try {
         await sequelize.authenticate();
         await sequelize.sync()
-        app.listen(PORT)
+        app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+        app.listen(PORT, () => console.log('Server listening on port'))
     } catch (error) {
         console.log(error.message)
     }  
 }
 start()
+
