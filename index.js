90dec3ce49950d757dfcd1b65da2d731b0304679
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-18 23:50:21 +0300

    ghdgfsfdg

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const sequelize = require('./db')
 const router = require('./routes/router')
 const PORT = 3000
 // swagger
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
+// const swaggerUi = require('swagger-ui-express');
+// const YAML = require('yamljs')
+// const swaggerDocument = YAML.load('./swagger.yaml')
 
 app.use(cors())
 app.use(express.json())
 app.use("/",router)
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const start = async() => {
     try {
         await sequelize.authenticate();
